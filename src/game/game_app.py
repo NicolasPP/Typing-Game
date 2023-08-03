34991a7d2b66b274eb9a575259c7e2ac512d58ae
Nicolas <nicolas.parucker@hotmail.com>
2023-08-03 17:37:43 +0200

    calculating delta time and passing it into update functions

--- src/game/game_app.py
+++ src/game/game_app.py
@@ -1,12 +1,14 @@
+from time import time
+
 from pygame import QUIT
 from pygame import display
 from pygame import event
 from pygame import font
 
-from game.game_screen import GameScreen
-from utils.themes import ThemeManager
-from game.game_state import GameState
 from game.components.letter import Letter
+from game.game_screen import GameScreen
+from game.game_state import GameState
+from utils.themes import ThemeManager
 
 
 class GameApp:
@@ -18,16 +20,22 @@ class GameApp:
 
         self.state: GameState = GameState(600, 600)
         self.done: bool = False
+        self.prev_time: float = 0.0
+        self.delta_time: float = 0.0
 
     def run(self) -> None:
 
         while not self.done:
+
+            now: float = time()
+            self.delta_time = now - self.prev_time
+            self.prev_time = now
 
             for game_event in event.get():
                 if game_event.type == QUIT:
                     self.done = True
 
             GameScreen.get().clear()
-            self.state.update()
+            self.state.update(self.delta_time)
             self.state.render()
             display.flip()
