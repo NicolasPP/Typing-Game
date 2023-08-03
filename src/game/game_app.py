2fb06aec9175f7f1b648c4e45367fad4ab383e12
Nicolas <nicolas.parucker@hotmail.com>
2023-08-03 17:32:21 +0200

    loading letter states colors and initializing the font module in game application init fucntion

--- src/game/game_app.py
+++ src/game/game_app.py
@@ -1,16 +1,20 @@
 from pygame import QUIT
 from pygame import display
 from pygame import event
+from pygame import font
 
 from game.game_screen import GameScreen
 from utils.themes import ThemeManager
 from game.game_state import GameState
+from game.components.letter import Letter
 
 
 class GameApp:
 
     def __init__(self) -> None:
         ThemeManager.load_themes()
+        Letter.load_state_colors()
+        font.init()
 
         self.state: GameState = GameState(600, 600)
         self.done: bool = False
