925741adf5eec4dda4dc78d026f70a9f7111910c
Nicolas <nicolas.parucker@hotmail.com>
2023-08-05 22:57:47 +0200

    moved spawning logic to game_state.py

--- src/game/game_state.py
+++ src/game/game_state.py
@@ -1,15 +1,23 @@
 from game.components.board import Board
 from game.components.word import WordManager
+from game.components.level import LevelManager
 
 
 class GameState:
 
     def __init__(self, board_width: int, board_height: int) -> None:
         self.board: Board = Board(board_width, board_height)
+        self.level_manager: LevelManager = LevelManager()
         self.word_manager: WordManager = WordManager()
 
     def render(self) -> None:
+        self.board.clear()
+        self.word_manager.render(self.board.surface)
         self.board.render()
 
+
     def update(self, delta_time: float) -> None:
-        self.word_manager.spawn_word(delta_time)
+        if self.word_manager.spawn_accumulator.wait(delta_time):
+            self.word_manager.spawn_word(self.level_manager.get_word_lengths())
+
+        self.word_manager.update(delta_time, self.level_manager.get_speed())
