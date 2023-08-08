5814e109cb4d3b5c54af8fbe6bfd62081a6498c8
Nicolas <nicolas.parucker@hotmail.com>
2023-08-08 19:42:45 +0200

    added constant variable WORD_COUNTER_FONT_SIZE

--- src/config/game_config.py
+++ src/config/game_config.py
@@ -5,10 +5,11 @@ BOARD_HEIGHT: int = 630
 LETTER_FONT_SIZE: int = 35
 LIFE_SURF_SIZE: int = 60
 SCORE_FONT_SIZE: int = 40
+WORD_COUNTER_FONT_SIZE: int = 20
 GUI_GAP: int = 5
-STARTING_SPEED: float = 50
+STARTING_SPEED: float = 30
 OUTLINE_THICKNESS: int = 5
-SPAWN_DELAY: float = 2  # Seconds
+SPAWN_DELAY: float = 3  # Seconds
 
 # -- files --
 WORDS_FILE: str = "data/words/words_alpha.txt"
