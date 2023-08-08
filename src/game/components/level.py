852138253d886f2cfb55c8e7d2ac719870d11505
Nicolas <nicolas.parucker@hotmail.com>
2023-08-08 15:21:26 +0200

    implemented set_completed_words which will also the GuiVars for score

--- src/game/components/level.py
+++ src/game/components/level.py
@@ -1,12 +1,16 @@
 from config.game_config import STARTING_SPEED
+from gui.gui_vars import GuiVars
 
 class LevelManager:
     def __init__(self) -> None:
         self.completed_words: int = 0
         self.speed: float = STARTING_SPEED
 
-    def increment_completed_words(self) -> None:
-        self.completed_words += 1
+    def set_completed_words(self, completed_words: int) -> None:
+        if completed_words < 0: return
+        if completed_words < self.completed_words: return
+        self.completed_words = completed_words
+        GuiVars.score.set(completed_words)
 
     def get_speed(self) -> float:
         return self.speed + (self.completed_words * 2)
