3b5e5b1eb3c2b40b6b1d1e2711692bc32d2dbfef
Nicolas <nicolas.parucker@hotmail.com>
2023-08-09 21:02:42 +0200

    class and script gui manager no longer needed

--- src/gui/pages/game_page.py
+++ src/gui/pages/game_page.py
@@ -1,29 +1,49 @@
+from typing import NamedTuple
+
 from pygame import KEYDOWN
+from pygame import Rect
 from pygame.event import Event
 
 from config.game_config import BOARD_HEIGHT
 from config.game_config import BOARD_WIDTH
 from game.game_state import GameState
+from game.game_stats import GameStats
 from gui.button_gui import ButtonEvent
-from gui.gui_manager import GuiManager
+from gui.game_over_gui import GameOverGui
+from gui.lives_gui import LivesGui
 from gui.pages.page import Page
+from gui.score_gui import ScoreGui
+
+
+class GamePageGui(NamedTuple):
+    lives: LivesGui
+    score: ScoreGui
+    game_over: GameOverGui
 
 
 class GamePage(Page):
+
+    @staticmethod
+    def get_gui(board_rect: Rect) -> GamePageGui:
+        return GamePageGui(LivesGui(board_rect), ScoreGui(board_rect), GameOverGui(board_rect))
+
     def __init__(self) -> None:
         self.state: GameState = GameState(BOARD_WIDTH, BOARD_HEIGHT)
-        self.gui_manager: GuiManager = GuiManager(self.state.board.rect)
-        self.gui_manager.game_over.retry_button.add_call_back(ButtonEvent.MOUSECLICK_LEFT, self.state.reset)
+        self.gui: GamePageGui = GamePage.get_gui(self.state.board.rect)
+        self.gui.game_over.retry_button.add_call_back(ButtonEvent.MOUSECLICK_LEFT, self.state.reset)
 
     def render(self) -> None:
         self.state.render()
-        self.gui_manager.render()
+        self.gui.lives.render()
+        self.gui.score.render()
+        if GameStats.get().game_over.get():
+            self.gui.game_over.render()
 
     def parse_event(self, event: Event) -> None:
         if event.type == KEYDOWN:
             self.state.process_key_name(event.key)
 
-        self.gui_manager.game_over.retry_button.parse_event(event)
+        self.gui.game_over.retry_button.parse_event(event)
 
     def update(self, delta_time: float) -> None:
         self.state.update(delta_time)
