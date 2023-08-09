b65c6f68c7f417419dc317cc335c8e9c9e974a9b
Nicolas <nicolas.parucker@hotmail.com>
2023-08-09 23:24:59 +0200

    added back button to game over gui to go back to main menu

--- src/gui/game_over_gui.py
+++ src/gui/game_over_gui.py
@@ -1,13 +1,15 @@
 from pygame import Rect
 from pygame import Surface
 
+from config.game_config import BACK_LABEL_VALUE
 from config.game_config import GAME_OVER_ALPHA
+from config.game_config import GUI_GAP
 from config.game_config import TRY_AGAIN_VALUE
-from game.game_screen import GameScreen
 from gui.button_gui import ButtonGui
 from gui.gui_component import GuiComponent
 from utils.themes import Theme
 from utils.themes import ThemeManager
+from utils.window import Window
 
 
 class GameOverGui(GuiComponent):
@@ -19,9 +21,17 @@ class GameOverGui(GuiComponent):
         theme: Theme = ThemeManager.get_theme()
         self.surface.fill(theme.background_primary)
 
+        self.back_button: ButtonGui = ButtonGui(BACK_LABEL_VALUE)
         self.retry_button: ButtonGui = ButtonGui(TRY_AGAIN_VALUE)
-        self.retry_button.rect.center = self.board_rect.center
+
+        self.back_button.rect.midright = self.board_rect.center
+        self.retry_button.rect.midleft = self.board_rect.center
+
+        self.back_button.rect.x -=  + GUI_GAP * 3
+        self.retry_button.rect.x +=  + GUI_GAP * 3
+
 
     def render(self) -> None:
-        self.retry_button.render(GameScreen.get_surface())
-        GameScreen.get_surface().blit(self.surface, self.board_rect)
+        self.retry_button.render(Window.get_surface())
+        self.back_button.render(Window.get_surface())
+        Window.get_surface().blit(self.surface, self.board_rect)
--- src/gui/pages/game_page.py
+++ src/gui/pages/game_page.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from typing import NamedTuple
 
 from pygame import KEYDOWN
@@ -13,6 +14,7 @@ from gui.game_over_gui import GameOverGui
 from gui.lives_gui import LivesGui
 from gui.pages.page import Page
 from gui.score_gui import ScoreGui
+from utils.window import Window
 
 
 class GamePageGui(NamedTuple):
@@ -27,10 +29,13 @@ class GamePage(Page):
     def get_gui(board_rect: Rect) -> GamePageGui:
         return GamePageGui(LivesGui(board_rect), ScoreGui(board_rect), GameOverGui(board_rect))
 
-    def __init__(self) -> None:
+    def __init__(self, change_page: Callable[[str], None]) -> None:
+        super().__init__(change_page)
         self.state: GameState = GameState(BOARD_WIDTH, BOARD_HEIGHT)
         self.gui: GamePageGui = GamePage.get_gui(self.state.board.rect)
         self.gui.game_over.retry_button.add_call_back(ButtonEvent.MOUSECLICK_LEFT, self.state.reset)
+        self.gui.game_over.back_button.add_call_back(ButtonEvent.MOUSECLICK_LEFT, self.state.reset)
+        self.gui.game_over.back_button.add_call_back(ButtonEvent.MOUSECLICK_LEFT, lambda: self.change_page("MenuPage"))
 
     def render(self) -> None:
         self.state.render()
@@ -43,7 +48,9 @@ class GamePage(Page):
         if event.type == KEYDOWN:
             self.state.process_key_name(event.key)
 
-        self.gui.game_over.retry_button.parse_event(event)
+        window_rect: Rect = Window.get_surface().get_rect()
+        self.gui.game_over.retry_button.parse_event(event, (window_rect.x, window_rect.y))
+        self.gui.game_over.back_button.parse_event(event, (window_rect.x, window_rect.y))
 
     def update(self, delta_time: float) -> None:
         self.state.update(delta_time)
