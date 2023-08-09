acd9534fde535d75a727c0814767cd1dee226ec1
Nicolas <nicolas.parucker@hotmail.com>
2023-08-09 23:24:06 +0200

    when instantiating a page you must pass PageManager's set_page function. So it can change pages.

--- src/gui/pages/page.py
+++ src/gui/pages/page.py
@@ -1,10 +1,14 @@
 from abc import ABC
 from abc import abstractmethod
+from typing import Callable
 
 from pygame.event import Event
 
 
 class Page(ABC):
+
+    def __init__(self, change_page: Callable[[str], None]) -> None:
+        self.change_page: Callable[[str], None] = change_page
 
     @abstractmethod
     def parse_event(self, event: Event) -> None:
