9baf7e9000cc82a3bd2338a0c63a4333325c5e2d
Nicolas <nicolas.parucker@hotmail.com>
2023-08-09 23:20:28 +0200

    pages in page manager are now indexed by their class name

--- src/gui/pages/page_manager.py
+++ src/gui/pages/page_manager.py
@@ -6,18 +6,18 @@ from gui.pages.page import Page
 class PageManager:
 
     def __init__(self) -> None:
-        self.pages: dict[Type[Page], Page] = {}
-        self.current_page: Type[Page] | None = None
+        self.pages: dict[str, Page] = {}
+        self.current_page: str | None = None
 
 
     def add_page(self, page: Type[Page]) -> None:
-        self.pages[page] = page()
+        self.pages[page.__name__] = page(self.set_page)
 
     def get_page(self) -> Page:
         assert self.current_page is not None, "current page not set"
-        assert self.current_page in self.pages, f"current page {self.current_page.__name__} does not exist"
+        assert self.current_page in self.pages, f"current page {self.current_page} does not exist"
         return self.pages[self.current_page]
 
-    def set_page(self, page: Type[Page]) -> None:
-        assert page in self.pages, f"current page {self.current_page.__name__} does not exist"
+    def set_page(self, page: str) -> None:
+        assert page in self.pages, f"current page {self.current_page} does not exist"
         self.current_page = page
