92679a9de36c9405a1730c291b8747f8dc6fc446
Nicolas <nicolas.parucker@hotmail.com>
2023-08-10 04:24:06 +0200

    added back button to score page

--- src/gui/pages/score_page.py
+++ src/gui/pages/score_page.py
@@ -2,18 +2,29 @@ from typing import Callable
 
 from pygame.event import Event
 
+from config.game_config import BACK_LABEL_VALUE
+from gui.button_gui import ButtonEvent
+from gui.button_gui import ButtonGui
 from gui.pages.page import Page
+from utils.themes import Theme
+from utils.themes import ThemeManager
+from utils.window import Window
 
 
 class ScorePage(Page):
     def __init__(self, change_page: Callable[[str], None]) -> None:
         super().__init__(change_page)
+        self.back_button: ButtonGui = ButtonGui(BACK_LABEL_VALUE)
+        self.back_button.rect.center = Window.get_surface().get_rect().center
+        self.back_button.add_call_back(ButtonEvent.LEFT_CLICK, lambda: self.change_page("MenuPage"))
+        theme: Theme = ThemeManager.get_theme()
+        self.back_button.configure(label_color=theme.foreground_primary, hover_color=theme.foreground_primary)
 
     def render(self) -> None:
-        pass
+        self.back_button.render(Window.get_surface())
 
     def parse_event(self, event: Event) -> None:
-        pass
+        self.back_button.parse_event(event)
 
     def update(self, delta_time: float) -> None:
         pass
