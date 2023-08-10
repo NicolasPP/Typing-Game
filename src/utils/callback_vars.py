5d90a323b026eff7da764057137c48ff1c176413
Nicolas <nicolas.parucker@hotmail.com>
2023-08-10 16:59:11 +0200

    added feature, to add a limit to how big a callback var can get

--- src/utils/callback_vars.py
+++ src/utils/callback_vars.py
@@ -8,10 +8,20 @@ class CallBack:
     def __init__(self, value: CallbackTypes) -> None:
         self.value: CallbackTypes = value
         self.callbacks: set[Callable[[CallbackTypes], None]] = set()
+        self.limit: CallbackTypes | None = None
+        # TODO add a way to add min and max not just max
 
     def set(self, value: CallbackTypes) -> None:
+        assert isinstance(value, type(self.value)), f"expected {type(self.value).__name__}, got {type(value).__name__}"
+        if self.limit is not None:
+            if value > self.limit: return
         self.value = value
         self.exec_callbacks()
+
+    def set_limit(self, limit: CallbackTypes) -> None:
+        assert isinstance(limit, type(self.value)), f"expected {type(self.value).__name__}, got {type(limit).__name__}"
+        print(limit)
+        self.limit = limit
 
     def exec_callbacks(self) -> None:
         for func in self.callbacks:
