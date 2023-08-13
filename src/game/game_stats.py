04d224ad913eefcd96a604679a0f29d804294c75
Nicolas <nicolas.parucker@hotmail.com>
2023-08-13 16:04:38 +0200

    added stats for level management

--- src/game/game_stats.py
+++ src/game/game_stats.py
@@ -10,15 +10,19 @@ from config.game_config import BASE_SPEED
 from config.game_config import BASE_COMBO_SPEED
 from config.game_config import BASE_COMBO_MULTIPLIER
 from config.game_config import MAX_COMBO_MULTIPLIER
+from config.game_config import BASE_WORDS_PER_LEVEL
+from config.game_config import BASE_WORD_LENGTH
+from config.game_config import BASE_TEXT_LENGTH
+from config.game_config import MAX_TEXT_LENGTH
 from utils.callback_vars import BoolCB
 from utils.callback_vars import FloatCB
 from utils.callback_vars import IntCB
-
+from utils.word_data_manager import WordDataManager
 
 @dataclass
 class Stats:
-    words_right: IntCB
-    words_wrong: IntCB
+    level_num: IntCB
+    words_required: IntCB
     life_pool: IntCB
     lives: IntCB
     spawn_delay: FloatCB
@@ -42,36 +46,38 @@ class GameStats:
 
     @staticmethod
     def get_base_stats() -> Stats:
-        words_right: IntCB = IntCB(0)
-        words_wrong: IntCB = IntCB(0)
+        level_num: IntCB = IntCB(0)
+        words_required: IntCB = IntCB(BASE_WORDS_PER_LEVEL)
         life_pool: IntCB = IntCB(BASE_LIFE_POOL)
         lives: IntCB = IntCB(BASE_LIVES_COUNT)
         spawn_delay: FloatCB = FloatCB(SPAWN_DELAY)
         fall_speed: FloatCB = FloatCB(BASE_SPEED)
-        word_length: IntCB = IntCB(2)
-        text_length: IntCB = IntCB(1)
+        word_length: IntCB = IntCB(BASE_WORD_LENGTH)
+        text_length: IntCB = IntCB(BASE_TEXT_LENGTH)
         game_over: BoolCB = BoolCB(False)
         combo_speed: FloatCB = FloatCB(BASE_COMBO_SPEED)
         combo_fill: FloatCB = FloatCB(0.0)
         combo_multiplier: FloatCB = FloatCB(BASE_COMBO_MULTIPLIER)
 
+        word_length.set_limit(BASE_WORD_LENGTH, max(WordDataManager.words_by_length.keys()))
+        text_length.set_limit(BASE_TEXT_LENGTH, MAX_TEXT_LENGTH)
         life_pool.set_limit(BASE_LIFE_POOL, MAX_LIFE_POOL)
         combo_multiplier.set_limit(BASE_COMBO_MULTIPLIER, MAX_COMBO_MULTIPLIER)
         lives.set_limit(None, BASE_LIFE_POOL)
         life_pool.add_callback(lambda val: lives.set_limit(None, val))
-        return Stats(words_right, words_wrong, life_pool, lives, spawn_delay, fall_speed, word_length, text_length,
+        return Stats(level_num, words_required, life_pool, lives, spawn_delay, fall_speed, word_length, text_length,
                      game_over, combo_speed, combo_fill, combo_multiplier)
 
     @staticmethod
     def reset(board_width: int) -> None:
-        GameStats.get().words_right.set(0)
-        GameStats.get().words_wrong.set(0)
+        GameStats.get().level_num.set(0)
+        GameStats.get().words_required.set(BASE_WORDS_PER_LEVEL)
         GameStats.get().life_pool.set(BASE_LIFE_POOL)
         GameStats.get().lives.set(BASE_LIVES_COUNT)
         GameStats.get().spawn_delay.set(SPAWN_DELAY)
         GameStats.get().fall_speed.set(BASE_SPEED)
-        GameStats.get().word_length.set(2)
-        GameStats.get().text_length.set(1)
+        GameStats.get().word_length.set(BASE_WORD_LENGTH)
+        GameStats.get().text_length.set(BASE_TEXT_LENGTH)
         GameStats.get().game_over.set(False)
         GameStats.get().combo_speed.set(BASE_COMBO_SPEED)
         GameStats.get().combo_fill.set(float(board_width))
